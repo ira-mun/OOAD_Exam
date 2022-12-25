5d7540a9e5e43a7852a31e3df560decde7691fcf
ira-mun <84065307+ira-mun@users.noreply.github.com>
2022-12-26 01:30:24 +0200

    Add files via upload

---
+++ DataProcessingInvoice.java
@@ -0,0 +1,31 @@
+public class DataProcessingInvoice{
+
+    public void calcScore(String tariff, String period, String payment){
+        //формування рахунку
+    }
+    public boolean autoPayment(String cardNumber, String expDate, String cvv, int currentDate){
+        //спроба списання коштів із картки через платіжний сервіс
+        if(/*вдалося списати кошти*/) return true;
+        else return false;
+    }
+    public void checkAutoPayment(String cardNumber, String expDate, String cvv, int currentDate){
+        //при невдачі автоматичної оплати відбувається декілька повторних спроб із інтервалом в день
+        if(autoPayment(cardNumber,expDate,cvv,currentDate) == true) {
+
+        }
+        else {
+            for(int i = 0; i <= 3; i++)
+            autoPayment(cardNumber,expDate,cvv,currentDate + i);
+            if( autoPayment(cardNumber,expDate,cvv,currentDate + 3) == false) blockCard(cardNumber,expDate,cvv);
+        }
+    }
+    public void blockCard(String cardNumber, String expDate, String cvv){
+        //блокування сервісу
+    }
+
+
+
+    
+
+
+}
---
+++ UserInvoice.java
@@ -0,0 +1,10 @@
+public class UserInvoice{
+    String tariff;//тариф
+    String period;//місяць, рік
+    String payment;//автоматична/ручна оплата
+    //дані банківської карти
+    String cardNumber;//номер карти
+    String expDate;//термін закінчення дії
+    String cvv;//cvv-код
+
+}
